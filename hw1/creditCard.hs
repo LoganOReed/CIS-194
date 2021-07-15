7ed4dd5db972386c726e6650e26f43359c13a044
orlogan <orlogan7@gmail.com>
2021-07-14 21:59:05 -0500

    Implemented validate

--- hw1/creditCard.hs
+++ hw1/creditCard.hs
@@ -3,7 +3,7 @@
 -- Goal: Validate a given credit card number using the following operations:
 --  1. Starting from the second to last digit and going backwards, double every other number
 --  2. Add every individual digit together (so if there is e.g a 15 in the list, we add 1 and 5 seperately)
---  3. Calculate the number from step 2 mod 10
+--  3. Calculate the number from step 2 mod 10, if it's 8 it is valid else kick it to the curb
 
 -- converts pos int to list of digits
 toDigits :: Integer -> [Integer]
@@ -30,9 +30,8 @@ doubleEveryOther xs = let (a:b:c) = reverse xs
                       in doubleEveryOther (reverse c) ++ b+b:[a]
 
 sumDigits :: [Integer] -> Integer
--- need recursion to be able to handle any sized int --
--- have first value be the accumulator and add next value to first if lt 10, else add mod 10 and recurse --
 
+-- technically overkill because it's only possible to have nums less than 100, but whatevs --
 sumDigits [] = 0
 sumDigits (x:[]) = x
 sumDigits (x:y:ns)
@@ -40,3 +39,15 @@ sumDigits (x:y:ns)
     | otherwise       = if y <= 10 
                         then sumDigits $ x+y:ns
                         else sumDigits $ x+(y `mod` 10):y `div` 10:ns
+
+validate :: Integer -> Bool
+
+-- Goal: Validate a given credit card number using the following operations:
+--  1. Starting from the second to last digit and going backwards, double every other number
+--  2. Add every individual digit together (so if there is e.g a 15 in the list, we add 1 and 5 seperately)
+--  3. Calculate the number from step 2 mod 10, if it's 8 it is valid else kick it to the curb
+
+-- uses prev helper functions to validate credit card --
+validate num = if (sumDigits . doubleEveryOther . toDigits $ num) `mod` 10 == 8
+               then True
+               else False
