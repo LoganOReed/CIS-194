7bcb0d3ef7f159094b82837ebf8af1b7c5884ccd
orlogan <orlogan7@gmail.com>
2021-07-16 23:58:53 -0500

    Implemented parseMessage

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -2,3 +2,38 @@
 module LogAnalysis where
 
 import Log
+
+-- Takes a validated string and returns a LogMessage type
+listToLogMessage :: [String] -> LogMessage
+
+listToLogMessage ("I":xs) =
+    LogMessage Info time str
+    where
+        time = read (head xs)::Int
+        str  = unwords $ drop 1 xs
+
+listToLogMessage ("W":xs) =
+    LogMessage Warning time str
+    where
+        time = read (head xs)::Int
+        str  = unwords $ drop 1 xs
+
+listToLogMessage ("E":n:xs) = 
+    LogMessage (Error severity) time str
+    where 
+        severity = read n :: Int
+        time     = read (head xs)::Int
+        str      = unwords $ drop 1 xs
+
+listToLogMessage str = 
+    Unknown $ unwords str
+
+
+-- Parses a string into the LogMessage Data Structure
+parseMessage :: String -> LogMessage
+
+parseMessage str =
+    listToLogMessage $ words str
+    
+
+
