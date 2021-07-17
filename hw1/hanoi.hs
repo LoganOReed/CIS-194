5d9beb9a9b181bfb06805ae8d12a33f5d6b7a157
orlogan <orlogan7@gmail.com>
2021-07-16 21:11:17 -0500

    Initialized HW2

--- hw1/hanoi.hs
+++ hw1/hanoi.hs
@@ -16,5 +16,5 @@ hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 
 hanoi 1 a _ c = [(a,c)]
 hanoi n a b c = 
-    (hanoi $ n - 1) a c b ++ (a,c):(hanoi $ n - 1) b a c
+    hanoi $ (n - 1) a c b ++ (a,c):hanoi $ (n - 1) b a c
 
