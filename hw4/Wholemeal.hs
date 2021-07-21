dd6524e3a21568d94d234b930a7e8ec7f44b939f
orlogan <orlogan7@gmail.com>
2021-07-21 00:39:34 -0500

    Initialized hw4

---
+++ hw4/Wholemeal.hs
@@ -0,0 +1,24 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Wholemeal where
+
+import Data.Foldable
+
+-----------------------------
+-- EXERCISE ONE:
+--  Reimplement the functions with wholemeal programming
+--  Principles
+-----------------------------
+
+-- multiply two less than all even numbers
+fun1 :: [Integer] -> Integer
+
+fun1 []         = 1
+fun1 (x:xs) 
+    | even x    = (x - 2) * fun1 xs
+    | otherwise = fun1 xs
+
+
+fun1' :: [Integer] -> Integer
+
+fun1' = foldr 1 . filter even
