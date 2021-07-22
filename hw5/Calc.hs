2a92ac98317a7d4e239402b4279d69578e0b72b5
orlogan <orlogan7@gmail.com>
2021-07-22 03:23:03 -0500

    Implemented evalStr

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -2,3 +2,31 @@
 
 module Calc where
 
+import ExprT
+import Parser
+
+------------------
+-- EXERCISE ONE --
+------------------
+
+-- Takes an ExprT and returns the int it
+--  evaluates to
+eval :: ExprT -> Integer
+eval (Lit x) = x
+eval (Add x y) = (eval x) + (eval y)
+eval (Mul x y) = (eval x) * (eval y)
+
+------------------
+-- EXERCISE TWO --
+------------------
+
+-- Maybe = Just a | Nothing
+
+-- Takes a arithmatic string and returns the 
+--  calculated expression
+evalStr :: String -> Maybe Integer
+evalStr str = case strExp of
+    Just x ->  Just (eval x)
+    Nothing  -> Nothing
+    where strExp = parseExp Lit Add Mul str
+
