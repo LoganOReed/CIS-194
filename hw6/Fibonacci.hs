3f69739fae1aaa4b3c3dc08986b67bd9bc69084f
orlogan <orlogan7@gmail.com>
2021-07-23 01:59:52 -0500

    Implemented Exercise 3

--- hw6/Fibonacci.hs
+++ hw6/Fibonacci.hs
@@ -30,3 +30,21 @@ fib2 a b = a : fib2 b (a + b)
 -- a lazy evaluation of fib, has to run in linear time
 fibs2 :: [Integer]
 fibs2 = fib2 0 1 
+
+--------------------
+-- EXERCISE THREE --
+--------------------
+
+-- List that is forced to be infinite
+data Stream a = Cons a (Stream a)
+
+-- helper to make a basic stream
+integralStream :: Integer -> Stream Integer
+integralStream n = Cons n (integralStream (n + 1))
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
+-- instance of show that allows us to show some of the stream
+instance Show a => Show (Stream a) where 
+    show = show . take 20 . streamToList
