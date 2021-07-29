df8b2ddfe04dc70c2d4ffd8e833aff56423610de
orlogan <orlogan7@gmail.com>
2021-07-29 03:57:15 -0500

    implemented glCons

--- hw8/Party.hs
+++ hw8/Party.hs
@@ -2,3 +2,9 @@
 
 module Party where
 
+import qualified Employee as E
+-- GL [Employee] Fun
+
+-- Adds an Employee to the GuestList
+glCons :: E.Employee -> E.GuestList -> E.GuestList
+glCons emp (E.GL gs f) = E.GL (emp:gs) (f + (E.empFun emp))
