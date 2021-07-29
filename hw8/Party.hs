7958b4a93ccff93e155416e505fc84f630052424
orlogan <orlogan7@gmail.com>
2021-07-29 04:05:27 -0500

    implemented monoid instance of guestlist

--- hw8/Party.hs
+++ hw8/Party.hs
@@ -1,9 +1,15 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-} --Needed since we create an instance of a module in Employee.hs
 
 module Party where
 
 import qualified Employee as E
--- GL [Employee] Fun
+
+instance Semigroup E.GuestList where 
+  (<>) (E.GL as funA) (E.GL bs funB) = (E.GL (as ++ bs) (funA + funB))
+
+instance Monoid E.GuestList where
+  mempty = E.GL [] 0
 
 -- Adds an Employee to the GuestList
 glCons :: E.Employee -> E.GuestList -> E.GuestList
